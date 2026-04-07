af49117489f09dd1160d2528e169628026ad21bb
TPSHION <tpshion@163.com>
2026-04-07 14:16:00 +0800

    Initial Commit

---
+++ parse/parseApp.swift
@@ -0,0 +1,17 @@
+//
+//  parseApp.swift
+//  parse
+//
+//  Created by chen on 2026/4/7.
+//
+
+import SwiftUI
+
+@main
+struct parseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
